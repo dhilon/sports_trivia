76c4dd130a5a5c7d09ffbfe914def45f9f17cba9
Kiran <phegaro@gmail.com>
2025-02-02 19:28:01 -0800

    Fixed this up. It was because tailwind went to v4 from v3. ShadCN was still using V3 so all the config files were broken

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,8 @@
-import tailwindcss from "@tailwindcss/vite"
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import tsconfigPaths from 'vite-tsconfig-paths'
-
+import tailwindcss from "@tailwindcss/vite"
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
   resolve: {
